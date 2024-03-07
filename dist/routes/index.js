b00552133d3041b4c71491334f8735dfa96d8d7f
RamKishore <mramki1803@gmail.com>
2024-03-07 08:36:04 +0530

    set up folder structure

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// routes/index.js
-const express_1 = require("express");
-const user_ts_1 = require("./user.ts"); // Make sure to include the file extension
-const router = express_1.default.Router();
-router.use('/user', user_ts_1.default);
-exports.default = router;
-//# sourceMappingURL=index.js.map
