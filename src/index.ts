0f5a846b7a02a3a39b122d34fa24173a3404fcb3
RamKishore <mramki1803@gmail.com>
2024-03-15 18:51:01 +0530

    docker compose, auto restart

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import rootRouter from './routes/index.js';
+import rootRouter from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,49 +1,24 @@
 import { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import { createUsersTable, addUser } from '../DB/db';
 
 const router = Router();
 
-import { Pool } from 'pg';
-
 router.use(bodyParser.json());
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'mysecretpassword',
-  port: 5432,
-});
-
-pool.query(
-  `CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    roll_number VARCHAR(20) NOT NULL
-  )`,
-  (err, res) => {
-    if (err) {
-      console.error('Error executing query', err.stack);
-    } else {
-      console.log('Table "users" successfully created or already exists');
-    }
-  }
-);
 
 router.post('/users', async (req: Request, res: Response) => {
   const { name, email, roll_number } = req.body;
 
   try {
+    // Ensure table exists
+    await createUsersTable();
+    
     // Insert data into the database
-    await pool.query('INSERT INTO users (name, email, roll_number) VALUES ($1, $2, $3)', [
-      name,
-      email,
-      roll_number,
-    ]);
+    await addUser(name, email, roll_number);
+
     res.status(201).send('User added successfully');
   } catch (error) {
-    console.error('Error executing query', error);
+    console.error('Error handling request:', error);
     res.status(500).send('Internal Server Error');
   }
 });
