73cd9e71621687d12f9d02fbd1280f71e944928e
RamKishore <mramki1803@gmail.com>
2024-04-19 15:53:53 +0530

    --1 change

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import rootRouter from './routes/index';
 import cookieParser from 'cookie-parser';
@@ -8,15 +8,8 @@ const app = express();
 const PORT = process.env.PORT;
 
 // Custom CORS middleware to handle credentials
-const corsOptions = {
-  origin: function (origin: any, callback: any) {
-    // Check if the origin is allowed
-    if (origin === 'http://localhost:5173' || origin === 'https://campus-connect-frontend-xi.vercel.app') {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+const corsOptions: cors.CorsOptions = {
+  origin: true, // Allow all origins
   credentials: true
 };
 
@@ -25,14 +18,20 @@ app.use(cookieParser());
 app.use(express.json());
 app.use('/api/v1', rootRouter);
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("<h1>Hello, you are in the root path</h1>")
 })
 
-app.get("/health", (req, res) => {
+app.get("/health", (req: Request, res: Response) => {
   res.status(200).json(`Server healthy on port ${PORT}.`);
   console.log(`Server healthy on port ${PORT}.`);
 })
+
+// Middleware to set Access-Control-Allow-Origin dynamically
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
