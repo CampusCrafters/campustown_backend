4fc622c9354d15543db1ff880af795fd25a62fd0
RamKishore <mramki1803@gmail.com>
2024-04-29 10:39:16 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,8 @@ const PORT = process.env.PORT;
 const corsOptions = {
   origin: [
     "http://localhost:5173",  // Your local frontend URL
-    "https://campus-connect-frontend-xi.vercel.app"  // Your production frontend URL
+    "https://campus-connect-frontend-xi.vercel.app",  // Your production frontend URL
+    "https://campusconnect-frontend.onrender.com"
   ],
   credentials: true
 };
