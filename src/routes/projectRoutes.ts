90be15e970e9051bf53172d1cabfb958f27e2048
RamKishore <mramki1803@gmail.com>
2024-05-01 12:17:34 +0530

    updated routes

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -5,13 +5,17 @@ import bodyParser from 'body-parser';
 const router = Router();
 router.use(bodyParser.json());
 
-// router.post('/addProject', addProjectService);
+// router.post('/postProject', postProjectService);
 // router.get('/projects', getAllProjectsService);
-
 // router.get('/myProjects', getMyProjectsService);
-// router.get('/applications', getApplicantsService);
+
 // router.post('/applyProject', addApplicationService);
+// router.put('/editApplication', editApplicationService);
+// router.delete('/deleteApplication', deleteApplicationService);
 
-
+// router.get('/projectApplicants', getProjectApplicantsService);
+// router.post('/acceptApplicant', acceptApplicantService);
+// router.post('/shortlistApplicant', shortlistApplicantService);
+// router.post('/rejectApplicant', rejectApplicantService);
 
 export default router;
