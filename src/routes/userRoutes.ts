39c4f6c1d4f85c8aa728778642314b135d4bd2ae
tharanithar-r <rtharanithar@gmail.com>
2024-05-03 12:26:22 +0530

    Added projects routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,24 +1,47 @@
+<<<<<<< Updated upstream
 import 'dotenv/config';
 import { Router } from 'express';
 import bodyParser from 'body-parser';
 import { authMiddleware } from '../middlewares/authMiddleware';
 import { signinService, getTokensAndStoreDataService, verifyTokenService } from '../service/auth/authService'
 import { viewProfileService, editProfileService, addProfileProjectService, viewProfileProjectService, editProfileProjectService, deleteProfileProjectService, viewExperienceService, addExperienceService, editExperienceService, deleteExperienceService } from '../service/user/userService'
+=======
+import "dotenv/config";
+import { Router } from "express";
+import bodyParser from "body-parser";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import {
+  signinService,
+  getTokensAndStoreDataService,
+  verifyTokenService,
+} from "../service/auth/authService";
+import {
+  viewProfileService,
+  editProfileService,
+  addProfileProjectService,
+  viewProfileProjectService,
+  editProfileProjectService,
+  deleteProfileProjectService,
+} from "../service/user/userService";
+>>>>>>> Stashed changes
 
 const router = Router();
 router.use(bodyParser.json());
+router.post("/gsignin", signinService);
+router.get("/oauth", getTokensAndStoreDataService);
+router.post("/verifyToken", verifyTokenService);
 
-router.post('/gsignin', signinService);
-router.get('/oauth', getTokensAndStoreDataService);
-router.post('/verifyToken', verifyTokenService);
+router.get("/viewProfile", authMiddleware, viewProfileService);
+router.put("/editProfile", authMiddleware, editProfileService);
 
-router.get('/viewProfile', authMiddleware, viewProfileService);
-router.put('/editProfile', authMiddleware, editProfileService);
-
-router.get('/viewProfileProject', viewProfileProjectService);
-router.post('/addProfileProject', authMiddleware, addProfileProjectService);
-router.put('/editProfileProject', authMiddleware, editProfileProjectService);
-router.delete('/deleteProfileProject', authMiddleware, deleteProfileProjectService);
+router.get("/viewProfileProject", viewProfileProjectService);
+router.post("/addProfileProject", authMiddleware, addProfileProjectService);
+router.put("/editProfileProject", authMiddleware, editProfileProjectService);
+router.delete(
+  "/deleteProfileProject",
+  authMiddleware,
+  deleteProfileProjectService
+);
 
 router.get('/viewExperience', viewExperienceService);
 router.post('/addExperience', authMiddleware, addExperienceService);
