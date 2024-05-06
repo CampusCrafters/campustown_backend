fde1a0c1361e8d0764a2af6c275a18cc0d157aaf
tharanithar-r <rtharanithar@gmail.com>
2024-05-07 00:50:54 +0530

    resume upload

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,14 +3,46 @@ import { Router } from "express";
 import bodyParser from "body-parser";
 import multer from "multer";
 import { authMiddleware } from "../middlewares/authMiddleware";
+<<<<<<< Updated upstream
 import { signinService, getTokensAndStoreDataService, verifyTokenService } from "../service/auth/authService";
 import { viewProfileService, editProfileService, addProfileProjectService, viewProfileProjectService, editProfileProjectService, deleteProfileProjectService, viewExperienceService, addExperienceService, editExperienceService, deleteExperienceService, getMyApplicationsService, profilePictureService, viewProfilePictureService } from "../service/user/userService";
+=======
+import {
+  signinService,
+  getTokensAndStoreDataService,
+  verifyTokenService,
+} from "../service/auth/authService";
+import {
+  viewProfileService,
+  editProfileService,
+  addProfileProjectService,
+  viewProfileProjectService,
+  editProfileProjectService,
+  deleteProfileProjectService,
+  viewExperienceService,
+  addExperienceService,
+  editExperienceService,
+  deleteExperienceService,
+  getMyApplicationsService,
+  addProfilePictureService,
+  viewProfilePictureService,
+  addResumeService,
+  viewResumeService,
+} from "../service/user/userService";
+>>>>>>> Stashed changes
 
 const router = Router();
 router.use(bodyParser.json());
 
+<<<<<<< Updated upstream
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage}); // hello
+=======
+//const upload = multer({ dest: "uploads/" });
+
+const storage: any = multer.memoryStorage();
+const upload = multer({ storage: storage });
+>>>>>>> Stashed changes
 
 router.post("/gsignin", signinService);
 router.get("/oauth", getTokensAndStoreDataService);
@@ -18,25 +50,43 @@ router.post("/verifyToken", verifyTokenService);
 
 router.get("/viewProfile", viewProfileService);
 router.put("/editProfile", authMiddleware, editProfileService);
+<<<<<<< Updated upstream
 router.post("/addProfilePicture", authMiddleware, upload.single('image'), profilePictureService);
 router.put("/editProfilePicture", authMiddleware, upload.single('image'), profilePictureService);
 router.delete("/deleteProfilePicture", authMiddleware, profilePictureService);
+=======
+router.post(
+  "/addProfilePicture",
+  authMiddleware,
+  upload.single("image"),
+  addProfilePictureService
+);
+>>>>>>> Stashed changes
 router.get("/viewProfilePicture", viewProfilePictureService);
 
-//router.post("/addResume", authMiddleware, addResumeService);
+router.post(
+  "/addResume",
+  authMiddleware,
+  upload.single("file"),
+  addResumeService
+);
 //router.delete("/deleteResume", authMiddleware, deleteResumeService);
-//router.get("/viewResume", viewResumeService);
+router.get("/viewResume", authMiddleware, viewResumeService);
 
 router.get("/viewProfileProject", viewProfileProjectService);
 router.post("/addProfileProject", authMiddleware, addProfileProjectService);
 router.put("/editProfileProject", authMiddleware, editProfileProjectService);
-router.delete("/deleteProfileProject", authMiddleware, deleteProfileProjectService);
+router.delete(
+  "/deleteProfileProject",
+  authMiddleware,
+  deleteProfileProjectService
+);
 
 router.get("/viewExperience", viewExperienceService);
 router.post("/addExperience", authMiddleware, addExperienceService);
 router.put("/editExperience", authMiddleware, editExperienceService);
 router.delete("/deleteExperience", authMiddleware, deleteExperienceService);
 
-router.get('/myApplications', authMiddleware, getMyApplicationsService);
+router.get("/myApplications", authMiddleware, getMyApplicationsService);
 
 export default router;
--- src/service/auth/authHelper.ts
+++ src/service/auth/authHelper.ts
@@ -1,92 +1,101 @@
-import axios from 'axios';
-import jwt from 'jsonwebtoken';
-import { addUser, checkEmailExists } from '../../DB/userDbFunctions';
-import { createUsersTable, createUserProjectsTable, createUserExperienceTable, createProjectsTable, createProjectApplicationsTable } from '../../DB/tables';
+import axios from "axios";
+import jwt from "jsonwebtoken";
+import { addUser, checkEmailExists } from "../../DB/userDbFunctions";
+import {
+  createUsersTable,
+  createUserProjectsTable,
+  createUserExperienceTable,
+  createProjectsTable,
+  createProjectApplicationsTable,
+} from "../../DB/tables";
 
 export const getUserInfoFromGoogle = async (access_token: string) => {
-    const response = await axios.post(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`);
-    return response.data;
+  const response = await axios.post(
+    `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`
+  );
+  return response.data;
 };
 
 export const generateJWT = (userInfo: any) => {
-    const user = {
-        email: userInfo.email,
-        name: userInfo.name,
-        rollNumber: userInfo.rollNumber,
-        batch: userInfo.batch,
-        branch: userInfo.branch
-    };
+  const user = {
+    email: userInfo.email,
+    name: userInfo.name,
+    rollNumber: userInfo.rollNumber,
+    batch: userInfo.batch,
+    branch: userInfo.branch,
+  };
 
-    if (!process.env.JWT_SECRET_KEY) {
-        throw new Error('JWT secret key is not defined');
-    }
+  if (!process.env.JWT_SECRET_KEY) {
+    throw new Error("JWT secret key is not defined");
+  }
 
-    return jwt.sign(user, process.env.JWT_SECRET_KEY, { expiresIn: '12h' }); 
+  return jwt.sign(user, process.env.JWT_SECRET_KEY, { expiresIn: "12h" });
 };
 
 export const verifyJWT = async (sessionToken: string) => {
-    try {
-        if (!process.env.JWT_SECRET_KEY) {
-            throw new Error('JWT secret key is not defined');
-        }
-        const decoded = await jwt.verify(sessionToken, process.env.JWT_SECRET_KEY);
-        if(decoded){
-            return decoded;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        throw new Error('Error verifying JWT (Unauthorized)');
+  try {
+    if (!process.env.JWT_SECRET_KEY) {
+      throw new Error("JWT secret key is not defined");
     }
+    const decoded = await jwt.verify(sessionToken, process.env.JWT_SECRET_KEY);
+    if (decoded) {
+      return decoded;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    throw new Error("Error verifying JWT (Unauthorized)");
+  }
 };
 
 export const storeUserData = async (userInfo: any) => {
-    try {
-        await createUsersTable();
-        await createUserProjectsTable();
-        await createUserExperienceTable();
-        await createProjectsTable();
-        await createProjectApplicationsTable();
-    
-        const Email = userInfo.email;
-        const exists = await checkEmailExists(Email);
-        if (!exists) {
-            const [rollNumber, year, Branch] = await generateRollNumber(Email);
-            const name = `${userInfo.name.replace(/ -IIITK$/, '').replace(/"/g, '')}`;
-            const email = Email;
-            const rollnumber = rollNumber;
-            const batch = parseInt(year, 10); // Convert year to a number
-            const branch = Branch;
+  try {
+    await createUsersTable();
+    await createUserProjectsTable();
+    await createUserExperienceTable();
+    await createProjectsTable();
+    await createProjectApplicationsTable();
 
-            await addUser(name, email, rollnumber, batch, branch); 
-        }
-    } catch (error) {
-        console.error('Error storing user data:', error);
-        throw new Error('Error storing user data');
+    const Email = userInfo.email;
+    const exists = await checkEmailExists(Email);
+
+    if (!exists) {
+      const [rollNumber, year, Branch] = await generateRollNumber(Email);
+      const name = `${userInfo.name.replace(/ -IIITK$/, "").replace(/"/g, "")}`;
+      const email = Email;
+      const rollnumber = rollNumber;
+      const batch = parseInt(year, 10); // Convert year to a number
+      const branch = Branch;
+
+      await addUser(name, email, rollnumber, batch, branch);
     }
+  } catch (error) {
+    console.error("Error storing user data:", error);
+    throw new Error("Error storing user data");
+  }
 };
 
 export const generateRollNumber = async (email: string) => {
-    const parts = email.split('@');   // Extracting relevant parts from the email
-    if (parts.length !== 2) {
-        throw new Error('Invalid email format');
-    }   // Ensure email format is valid
-    const username = parts[0];   // Extracting the branch  from the username
-    const branchMatch = username.match(/\d{2}([a-zA-Z]+)/);
-    const branch = branchMatch ? branchMatch[1].toUpperCase() : '';
-    if (!branch) {
-        throw new Error('Branch not found in username');
-    }   // Ensure branch is extracted
-    const yearMatch = username.match(/\d{2}/);  // Extracting the year from the username
-    const year = yearMatch ? `20${yearMatch[0]}` : '';
-    if (!year) {
-        throw new Error('Year not found in username');
-    }   // Ensure year is extracted
-    const rollNumberMatch = username.match(/\d{1,4}$/);   // Extracting the roll number suffix from the username
-    if (!rollNumberMatch) {
-        throw new Error('Roll number suffix not found in username');
-    }   // Ensure roll number suffix is extracted
-    const rollNumberSuffix = rollNumberMatch[0];
-    const rollNumber = `${year}${branch}${rollNumberSuffix.padStart(4, '0')}`;   // Formatting the roll number
-    return [rollNumber, year, branch];
-}
+  const parts = email.split("@"); // Extracting relevant parts from the email
+  if (parts.length !== 2) {
+    throw new Error("Invalid email format");
+  } // Ensure email format is valid
+  const username = parts[0]; // Extracting the branch  from the username
+  const branchMatch = username.match(/\d{2}([a-zA-Z]+)/);
+  const branch = branchMatch ? branchMatch[1].toUpperCase() : "";
+  if (!branch) {
+    throw new Error("Branch not found in username");
+  } // Ensure branch is extracted
+  const yearMatch = username.match(/\d{2}/); // Extracting the year from the username
+  const year = yearMatch ? `20${yearMatch[0]}` : "";
+  if (!year) {
+    throw new Error("Year not found in username");
+  } // Ensure year is extracted
+  const rollNumberMatch = username.match(/\d{1,4}$/); // Extracting the roll number suffix from the username
+  if (!rollNumberMatch) {
+    throw new Error("Roll number suffix not found in username");
+  } // Ensure roll number suffix is extracted
+  const rollNumberSuffix = rollNumberMatch[0];
+  const rollNumber = `${year}${branch}${rollNumberSuffix.padStart(4, "0")}`; // Formatting the roll number
+  return [rollNumber, year, branch];
+};
