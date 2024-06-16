65c3b24a7a3082d6138cac1c3cfadf66be5b3e6c
RamKishore <ramkishore@RamKishores-MacBook.local>
2024-06-16 17:09:17 +0530

    .

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,19 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import path from "path";
 import userRouter from "./userRoutes";
 import projectRouter from "./projectRoutes";
 
 const router = express.Router();
 
+// User routes
 router.use("/user", userRouter);
+
+// Project routes
 router.use("/project", projectRouter);
+
+// Default route to handle unmatched routes
+router.use("*", (req: Request, res: Response) => {
+  res.status(404).sendFile(path.join(__dirname, '../../404.html'));
+});
 
 export default router;
